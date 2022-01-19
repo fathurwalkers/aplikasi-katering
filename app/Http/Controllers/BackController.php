7b24d97a7837f2812d9225de4373402e720cf850
Fathur Walkers <fathurwalkers44@gmail.com>
2022-01-18 16:20:15 -0800

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
 use App\Models\Login;
 
 class BackController extends Controller
@@ -24,11 +29,76 @@ class BackController extends Controller
 
     public function postlogin(Request $request)
     {
-        //
+        $cariUser = Login::where('login_username', $request->login_username)->get();
+        if ($cariUser->isEmpty()) {
+            return back()->with('status', 'Maaf username atau password salah!')->withInput();
+        }
+        $data_login = Login::where('login_username', $request->login_username)->firstOrFail();
+        switch ($data_login->login_level) {
+            case 'admin':
+                $cek_password = Hash::check($request->login_password, $data_login->login_password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard');
+                    }
+                }
+                break;
+            case 'bidan':
+                $cek_password = Hash::check($request->login_password, $data_login->login_password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard');
+                    }
+                }
+                break;
+            case 'user':
+                $cek_password = Hash::check($request->login_password, $data_login->login_password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard');
+                    }
+                }
+                break;
+        }
+        return back()->with('
+        ', 'Maaf username atau password salah!')->withInput();
     }
 
     public function postregister(Request $request)
     {
-        //
+        $login_data = new Login;
+        $validatedLogin = $request->validate([
+            'login_nama' => 'required',
+            'login_username' => 'required',
+            'login_password' => 'required',
+            'login_email' => 'required',
+            'login_telepon' => 'required'
+        ]);
+        $hashPassword = Hash::make($validatedLogin["login_password"], [
+            'rounds' => 12,
+        ]);
+        $token_raw = Str::random(16);
+        $token = Hash::make($token_raw, [
+            'rounds' => 12,
+        ]);
+        $level = "user";
+        $login_status = "verified";
+        $login_data = Login::create([
+            'login_nama' => $validatedLogin["login_nama"],
+            'login_username' => $validatedLogin["login_username"],
+            'login_password' => $hashPassword,
+            'login_email' => $validatedLogin["login_email"],
+            'login_telepon' => $validatedLogin["login_telepon"],
+            'login_token' => $token,
+            'login_level' => $level,
+            'login_status' => $login_status,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $login_data->save();
+        return redirect()->route('login')->with('status', 'Berhasil melakukan registrasi!');
     }
 }
