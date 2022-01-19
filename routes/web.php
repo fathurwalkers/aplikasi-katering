d216548608e6c965a14ff5ab1d442fc57259186a
Fathur Walkers <fathurwalkers44@gmail.com>
2022-01-18 16:29:11 -0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
+})->name('home');
+
+Route::get('/login', [BackController::class, 'login'])->name('login');
+Route::get('/register', [BackController::class, 'register'])->name('register');
+Route::post('/post-login', [BackController::class, 'post_login'])->name('postlogin');
+Route::post('/post-register', [BackController::class, 'post_register'])->name('postregister');
+Route::post('/logout', [BackController::class, 'logout'])->name('logout');
+
+Route::group(['prefix' => '/dashboard'], function () {
+    Route::get('/', [BackController::class, 'index'])->name('dashboard');
 });
