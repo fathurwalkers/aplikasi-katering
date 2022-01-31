a76d4bc2f86b44b61a253e85fe74353968cefcbb
Fathur Walkers <fathurwalkers44@gmail.com>
2022-01-30 16:36:52 -0800

    update

--- app/Models/Informasi.php
+++ app/Models/Informasi.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Laporan extends Model
+class Informasi extends Model
 {
     use HasFactory;
-
-    protected $table = 'laporan';
-    protected $primaryKey = 'id';
-    protected $guarded = [];
 }
