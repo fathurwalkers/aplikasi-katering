8e522c09b002673392825b3eacfc8be5ad01c8cc
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-01 09:05:34 -0800

    update

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -82,7 +82,41 @@
 
     <!-- footer -->
     <footer id="footer1" class="footer1 col-12 fixed-bottom py-2">
-        <x-client-navbar-footer />
+        <ul class="nav justify-content-around text-white">
+            <li class="nav-item">
+              <a class="nav-link active text-white py-0 d-flex flex-column" aria-current="page" href="{{ route('dashboard') }}"
+                ><i class="fas fa-home my-auto mx-auto"></i>
+                <p class="mb-0">Beranda</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active text-white py-0 d-flex flex-column" aria-current="page" href="{{ route('client-daftar-paket') }}"
+                ><i class="fas fa-list my-auto mx-auto"></i>
+                <p class="mb-0">Daftar Paket</p>
+              </a>
+            </li>
+
+            @php
+                $users = session('data_login');
+            @endphp
+            @if ($users)
+            <li class="nav-item">
+              <a class="nav-link active text-white py-0 d-flex flex-column" aria-current="page" href="{{ route('dashboard') }}"
+                ><i class="fas fa-users my-auto mx-auto"></i>
+                <p class="mb-0">Dashboard</p>
+              </a>
+            </li>
+            @endif
+            @if ($users == null)
+            <li class="nav-item">
+              <a class="nav-link active text-white py-0 d-flex flex-column" aria-current="page" href="{{ route('login') }}"
+                ><i class="fas fa-users my-auto mx-auto"></i>
+                <p class="mb-0">Masuk</p>
+              </a>
+            </li>
+            @endif
+
+        </ul>
     </footer>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
