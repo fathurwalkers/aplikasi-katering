d8b73b2fcf662df4dec929e0183e203f77bb9335
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-24 06:52:58 -0700

    update

--- resources/views/admin/daftar-paket.blade.php
+++ resources/views/admin/daftar-paket.blade.php
@@ -17,7 +17,16 @@
         <div class="card">
 
             <div class="card-header bg-primary">
-                <h4>Daftar Paket</h4>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-6 col-lg-6">
+                            <h4>Daftar Paket</h4>
+                        </div>
+                        <div class="col-sm-6 col-md-6 col-lg-6">
+                            <button type="button" class="btn btn-md btn-success float-right" data-toggle="modal" data-target="#modaltambahdata">TAMBAH PAKET</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="card-body">
@@ -69,6 +78,76 @@
                                 </div>
                             </td>
                         </tr>
+
+                        {{-- MODAL TAMBAH DATA  --}}
+                        <div class="modal fade" id="modaltambahdata" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Paket</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <form action="#" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="modal-body">
+
+                                        <div class="row">
+                                            <div class="col-sm-4 col-md-4 col-lg-4">
+                                                <div class="form-group">
+                                                    <label for="paket_nama">Nama Paket </label>
+                                                    <input type="text" class="form-control" id="paket_nama" name="paket_nama">
+                                                    {{-- <small id="emailHelp" class="form-text text-muted">Test</small> --}}
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4 col-md-4 col-lg-4">
+                                                <div class="form-group">
+                                                    <label for="paket_harga">Harga </label>
+                                                    <input type="number" class="form-control" id="paket_harga" name="paket_harga">
+                                                    {{-- <small id="emailHelp" class="form-text text-muted">Test</small> --}}
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4 col-md-4 col-lg-4">
+                                                <div class="form-group">
+                                                    <label for="paket_status">Status Paket </label>
+                                                    <select class="form-control" id="paket_status" name="paket_status">
+                                                      <option value="" selected>Pilih status paket...</option>
+                                                      <option value="TERSEDIA" selected>Tersedia</option>
+                                                      <option value="KOSONG" selected>Kosong</option>
+                                                    </select>
+                                                  </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                                <div class="form-group">
+                                                    <label for="paket_gambar">Banner Gambar</label>
+                                                    <input type="file" class="form-control-file" id="paket_gambar" name="paket_gambar">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                                <textarea name="paket_info" id="editordefault">
+                                                </textarea>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-info" data-dismiss="modal">Batalkan</button>
+                                        <button type="submit" class="btn btn-danger">Ubah Data</button>
+                                    </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                        {{-- END MODAL TAMBAH DATA  --}}
 
                         {{-- MODAL UBAH  --}}
                         <div class="modal fade" id="modalubah{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -139,6 +218,7 @@
                                 </div>
                             </div>
                         </div>
+                        {{-- END MODAL UBAH  --}}
 
                         {{-- MODAL HAPUS  --}}
                         <div class="modal fade" id="modalhapus{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -184,6 +264,7 @@
     $(document).ready( function () {
         $('#example').DataTable();
         <?php foreach ($paket as $item) { ?>
+        CKEDITOR.replace( "editordefault" );
         CKEDITOR.replace( "editor<?php echo $item->id; ?>" );
         <?php } ?>
         CKEDITOR.document.appendStyleText( '.cke{visibility:visible;}' );
