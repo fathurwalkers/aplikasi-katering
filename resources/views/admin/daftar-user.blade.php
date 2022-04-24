616ae4a2d51a1ed51097d7701139be007c0a96eb
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-24 14:56:22 -0700

    update

--- resources/views/admin/daftar-user.blade.php
+++ resources/views/admin/daftar-user.blade.php
@@ -44,7 +44,7 @@
                             <td width="15%">
                                 <div class="btn-group d-flex justify-content-center">
                                 <a href="#" class="btn btn-info btn-sm mr-1">LIHAT</a>
-                                    <a href="#" class="btn btn-primary btn-sm mr-1">UBAH</a>
+                                    {{-- <a href="#" class="btn btn-primary btn-sm mr-1">UBAH</a> --}}
                                     <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalhapus{{ $item->id }}">
                                         HAPUS
                                     </button>
