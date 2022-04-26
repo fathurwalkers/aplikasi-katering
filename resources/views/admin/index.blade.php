586c1e700e6edb82789e82f0fd2a40ae3c65ca1b
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-26 07:30:45 -0700

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,12 +6,12 @@
 
 @endsection
 
-@section('main-header', 'Headers')
+@section('main-header', 'Dashboard Panel')
 
 @section('main-content')
 
     <div class="row">
-        <div class="col-12">
+        <div class="col-sm-12 col-md-12 col-lg-12">
             <div class="card">
                 Header Layoput
             </div>
