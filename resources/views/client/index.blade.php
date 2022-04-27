93fe22a60115f5c38a1b5e6983da85b97b7d4cb0
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-27 14:08:02 -0700

    update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -6,6 +6,11 @@
     background-color: #2bc75a;
     color: #ffffff;
 }
+
+.fullbutton {
+    width: 100%!important;
+    padding: 0.5rem 8rem!important;
+}
 </style>
 @endsection
 
@@ -51,15 +56,18 @@
                                 &nbsp;&nbsp;&nbsp; DAFTAR PENGGUNA
                             </button>
                         </div>
-                    </div>
+                    </div> --}}
                     <div class="row">
                         <div class="col-12 d-flex justify-content-center">
-                            <button class="btn btn-lg btn-info btn-block mb-2" style="width:100%">
-                                <i class="fas fa-archive"></i>
-                                &nbsp;&nbsp;&nbsp; DAFTAR PENGGUNA
-                            </button>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-lg btn-info btn-block mb-2 fullbutton" style="width:100%!important;">
+                                    <i class="fas fa-archive"></i>
+                                    &nbsp;&nbsp;&nbsp; LOGOUT
+                                </button>
+                            </form>
                         </div>
-                    </div> --}}
+                    </div>
                 {{-- </div> --}}
             {{-- </div> --}}
         {{-- </div> --}}
